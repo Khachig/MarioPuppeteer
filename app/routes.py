a811df6f442fd36b004bbf0f9ff34481d8ef0ea8
Khachig Ainteblian <ainteblian99@gmail.com>
2019-06-15 01:48:52 -0400

    Added game commands
    Added game commands and write_command method to write corresponding
    memory address/value to the file that is to be read by the lua script

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,47 @@
 from flask import render_template, flash, request, redirect, url_for
 from wtforms import Form, TextField, validators
 from app import app
+from os import path
 
 
 class CommandForm(Form):
     # Command field
     command = TextField("Command:", validators=[validators.required()])
 
+    @staticmethod
+    def write_command(cmd):
+        # Game commands and their corresponding memory address/value pairs
+        mario_commands = {'jump':'0x009F 0xFB', 'highjump':'0x009F 0xF8', 'time':'0x07F8 0x09',
+                          'lives':'0x075A 0x80', 'big':'0x0754 0x00'}
+
+        # Path to root directory of app
+        root_directory = path.dirname(app.root_path)
+        filepath = path.join(root_directory, "luascript", "commands.txt")
+
+        with open(filepath, "w") as cmd_file:
+            # Try to write the corresponding memory address/value to commands.txt
+            try:
+                cmd_file.write(mario_commands[cmd])
+                cmd_file.write("\n")
+                flash(f"Command [{cmd}] written to memory!")
+                return 0
+            except KeyError:
+                flash(f"Command [{cmd}] not found!")
+                return 1
+
+
     @app.route("/", methods=["GET", "POST"])
     @app.route("/index")
     def index():
+
+        # Form object
         form = CommandForm(request.form)
 
         if request.method == "POST":
             command = request.form["command"]
-            # Perform field validation, then flash field content
+            # Perform field validation, then write command to file
             if form.validate():
-                flash("Command: " + command)
+                CommandForm.write_command(command)
                 # Redirect back to same page with empty field
                 return redirect(url_for("index"))
 

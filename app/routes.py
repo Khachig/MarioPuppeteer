ce2ed4ffeef5ea3f7f73a55b2251871a0e26e9ed
Khachig Ainteblian <ainteblian99@gmail.com>
2019-06-15 01:05:08 -0400

    Added form and html template
    Added form text and submit fields and form handling.
    Added index.html template

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,27 @@
+from flask import render_template, flash, request, redirect, url_for
+from wtforms import Form, TextField, validators
 from app import app
 
-@app.route("/")
-def index():
-    return "Hello, World!"
+
+class CommandForm(Form):
+    # Command field
+    command = TextField("Command:", validators=[validators.required()])
+
+    @app.route("/", methods=["GET", "POST"])
+    @app.route("/index")
+    def index():
+        form = CommandForm(request.form)
+
+        if request.method == "POST":
+            command = request.form["command"]
+            # Perform field validation, then flash field content
+            if form.validate():
+                flash("Command: " + command)
+                # Redirect back to same page with empty field
+                return redirect(url_for("index"))
+
+        # Render index.html
+        return render_template("index.html", form=form)
 
 
 if __name__ == "__main__":

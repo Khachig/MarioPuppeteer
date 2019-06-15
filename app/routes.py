24510a56cd7b97bd5bc653723bee4f71586a8c8b
Khachig Ainteblian <ainteblian99@gmail.com>
2019-06-15 00:52:34 -0400

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from app import app
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+import subprocess as sp
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+
